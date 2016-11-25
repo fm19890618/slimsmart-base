9112cf3ef68548a22c34eb47dbdb9cc881f15a60
fangming <fangming@asrsc.com>
2016-11-25 17:20:45 +0800

    声音app

---
+++ slimsmart-parent/slimsmart-api/src/main/java/com/slimsmart/api/bus/voice/VoiceLocationService.java
@@ -0,0 +1,8 @@
+package com.slimsmart.api.bus.voice;
+
+import com.slimsmart.common.service.BaseService;
+import com.slimsmart.model.bus.voice.VoiceLocation;
+
+public interface VoiceLocationService extends BaseService<VoiceLocation> {
+
+}
---
+++ slimsmart-parent/slimsmart-api/src/main/java/com/slimsmart/api/bus/voice/VoiceService.java
@@ -0,0 +1,12 @@
+package com.slimsmart.api.bus.voice;
+
+import java.util.List;
+import java.util.Map;
+
+import com.slimsmart.common.model.VoiceQueryVo;
+import com.slimsmart.common.service.BaseService;
+import com.slimsmart.model.bus.voice.Voice;
+
+public interface VoiceService extends BaseService<Voice> {
+	public List<Map<String,Object>> getLocationAndVoice(VoiceQueryVo vo);
+}
